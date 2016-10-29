a72023d4649c25a01008a6fdb6dd11b19d1a6840
Jackjan <Jackjan@protonmail.com>
2016-10-29 22:54:04 +0200

    Fixed a missing inbound package
    Added the inbound package "ResetPlayer.cs" for removing a player from the PlayerPosDB if he dies on another server

---
+++ Hive/Oxide.Ext.Hive/Oxide.Ext.Hive.Net/Oxide.Ext.Hive.Net.Inbound/ResetPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Oxide.Ext.Hive.Net.Answers;
+
+namespace Oxide.Ext.Hive.Answers {
+	public class ResetPlayer : BaseAnswer {
+
+		public ulong steamid;
+
+		public ResetPlayer()
+		{
+			
+		}
+
+		public override void function(string id) {
+			PlayerPosDB.getInstance().PopUser(steamid);
+		}
+
+
+	}
+}
+
