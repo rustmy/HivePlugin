0cca6dbfd3cd6fdf86c5edc3b58e20bb835534ae
Jackjan <Jackjan@protonmail.com>
2016-10-29 18:37:31 +0200

    Workings on 0.8.9
    Included a new syncing addition which lets user disabled/enable syncing of blueprings/inventory/exp

---
+++ Hive/Oxide.Ext.Hive/Oxide.Ext.Hive.Plugin/OxideCorePlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Ext.Hive.Plugins
+{
+	public abstract class OxideCorePlugin : CSPlugin
+	{
+		
+		public OxideCorePlugin()
+		{
+			
+		}
+
+
+
+
+
+	}
+}
+
